94a90a89a9726b86d69a3fd0160936709143d8d2
gonzaloev <gonzalo.valino@istea.com.ar>
2021-11-17 01:58:59 -0300

    archivo js actualizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,38 @@
-$function()({
+$(function(){
 
-    
+    let productoBase = $(".plantilla .producto");
 
-});
+    function buscar_productos() {
+        console.log("BUSCANDO PRODUCTOS");
 
+        $.ajax({
+            url: 'https://my-json-server.typicode.com/srsanma/terranovaapirest/propiedades',
+            type: 'GET',
+            dataType: 'json',
+
+            success: function(data){ // Funcion de callback
+                console.log("RESPUESTA OK");
+                console.log(data);
+
+                for (producto of data) {
+                    let nuevoProducto = productoBase.clone();
+
+                    nuevoProducto.find(".nombre").text(producto.name);
+                    nuevoProducto.find(".edad").text(producto.age);
+                    nuevoProducto.find(".id").text(producto.id);
+
+                    $("#ProductosContenedor").append(nuevoProducto);
+                }
+            },
+
+            error: function(request, error) {
+                console.log("Ocurrio un error: " + error);
+            }
+        });
+    }
+
+    buscar_productos();
+})
 /* 
 objeto propiedad
 img-propiedad => url de la foto 
